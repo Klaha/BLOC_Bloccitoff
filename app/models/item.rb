154410c1f499d0d5308f8cb360b3962d8ba9d41e
Klaha <klaha.77@gmail.com>
2015-04-12 19:05:30 -0430

    Display Time Remaining

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,12 @@ class Item < ActiveRecord::Base
   default_scope { order('updated_at DESC') }
 
   validates :name, presence: true
+
+  def days_left_text
+    "#{self.days_left} days left to complete!"
+  end
+
+  def days_left
+    7 - (DateTime.now.to_date - created_at.to_date).to_i
+  end
 end

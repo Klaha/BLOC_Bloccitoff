bbe73bb906699abec2b7205bc5310874c9a93e31
Klaha <klaha.77@gmail.com>
2015-04-13 13:32:40 -0430

    Responsiveness :octocat: and Final Commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,8 @@
 //= require jquery_ujs
 //= require_tree .
 $(document).ready(function(){
+  $(".button-collapse").sideNav();
+  
   $("#close").click(function(){
     $("#success").fadeOut('slow');
   });
